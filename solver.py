bfadce7dc83ae77c0e91e1415cc6d2d4586222db
jrdzha <jared@jaredzhao.com>
2018-11-30 00:45:27 -0800

    few changes

--- solver.py
+++ solver.py
@@ -246,7 +246,7 @@ def main():
         for input_folder in os.listdir(category_dir):
             input_name = os.fsdecode(input_folder)
             if int(input_name) % int(sys.argv[1]) == int(sys.argv[2]):
-                print("Solving: ", input_name)
+                print("Solving: ", size, input_name)
                 graph, num_buses, size_bus, constraints = parse_input(category_path + "/" + input_name)
                 solution = solve(graph, num_buses, size_bus, constraints)
                 output_file = open(output_category_path + "/" + input_name + ".out", "w")
